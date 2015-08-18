ac7ede0089e9bee0155fa50760609c5bbb1b372c
bayesrule <bayesrule@outlook.com>
2015-08-18 12:42:02 +0100

    cleaning

--- lab/twirlingvr_web/assets/css/ie9.css
+++ lab/twirlingvr_web/assets/css/ie9.css
@@ -1,6 +1,7 @@
 /*
-	Landed by HTML5 UP
-	html5up.net | @n33co
+	Modeified by Hao Liu (haoliu.cc)
+	originally by
+	@n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
